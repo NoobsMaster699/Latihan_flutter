9a06da41fb7525ee32b1b9264c5c9f54b42c01b1
NoobsMaster699 <muhammad.reno.ti.21@cic.ac.id>
2023-10-17 10:51:52 +0700

    ubah

--- lib/main.dart
+++ lib/main.dart
@@ -1,60 +1,67 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
-
+ 
 void main() {
   runApp(MaterialApp(
     home: MyApp(),
   ));
 }
+ 
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+ 
+class _MyAppState extends State<MyApp> {
 
-class MyApp extends StatelessWidget {
+  final String apiUrl = "http://192.168.100.168/latihan/data.php";
+  
+ 
   Future<List<dynamic>> fetchData() async {
-    final response = await http.get(Uri.parse("http://localhost/latihan/data.php"));
+    final response = await http.get(Uri.parse(apiUrl));
+ 
     if (response.statusCode == 200) {
-      List<dynamic> data = json.decode(response.body);
+      final data = json.decode(response.body)['data'];
       return data;
     } else {
       throw Exception('Failed to load data');
     }
   }
-
+ 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Belajar API'),
+        title: Text('BELAJAR API'),
       ),
-      body: Container(
-        child: FutureBuilder(
-          future: fetchData(),
-          builder: (BuildContext context, AsyncSnapshot snapshot) {
-            if (snapshot.connectionState == ConnectionState.done) {
-              if (snapshot.hasError) {
-                return Center(
-                  child: Text('Error: ${snapshot.error}'),
+      body: FutureBuilder<List<dynamic>>(
+        future: fetchData(),
+        builder: (BuildContext context, AsyncSnapshot<List<dynamic>> snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Center(child: Text('Error: ${snapshot.error}'));
+          } else {
+            return ListView.builder(
+              padding: EdgeInsets.all(10),
+              itemCount: snapshot.data?.length,
+              itemBuilder: (BuildContext context, int index) {
+                return ListTile(
+                  leading: CircleAvatar(
+                    radius: 30,
+                    backgroundImage: Image.network('http://192.168.100.168/latihan/img/' + snapshot.data![index]['avatar'])
+                        .image,
+                  ),
+                  title: Text(snapshot.data![index]['first_name'] + " " + snapshot.data![index]['last_name']),
+                  subtitle: Text(snapshot.data![index]['produk']),
                 );
-              } else {
-                List<dynamic> data = snapshot.data;
-                return ListView.builder(
-                  itemCount: data.length,
-                  itemBuilder: (BuildContext context, int index) {
-                    return ListTile(
-                      leading: Image.network('http://localhost/latihan/' + data[index]['avatar']),
-                      title: Text(data[index]['first_name'] + ' ' + data[index]['last_name']),
-                      subtitle: Text(data[index]['produk']),
-                    );
-                  },
-                );
-              }
-            } else {
-              return Center(
-                child: CircularProgressIndicator(),
-              );
-            }
-          },
-        ),
+              },
+            );
+          }
+        },
       ),
     );
   }
 }
+ 
